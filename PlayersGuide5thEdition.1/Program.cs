7a107f58171b491ec44ee0fdd3496ab2d7c54f34
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2022-10-07 10:54:26 +0200

    Add project files.

---
+++ PlayersGuide5thEdition.1/Program.cs
@@ -0,0 +1,33 @@
+﻿//challenge Hello, World! 50XP 
+
+using System.Data;
+
+Console.WriteLine(" Hello, World! ");
+
+//challenge What Comes Next 50XP
+Console.WriteLine("My name is Quan.");
+
+//challenge The Makings of a Programmer 50XP
+Console.WriteLine("I have come from the far away land... ");
+Console.WriteLine("This place is such a beautiful place. ");
+Console.WriteLine("Do you mind answering my question? ");
+Console.WriteLine("What is your name? ");
+string name = Console.ReadLine();
+Console.WriteLine(" Hello " + name + "! Nice to meet you! ");
+
+//challenge Consolas and Telim 50XP 
+Console.WriteLine("\nBread is ready.\n Who is the bread for? "); 
+string breadTaker = Console.ReadLine();
+Console.WriteLine("Noted: " + breadTaker + " got bread.");
+
+//challenge The Thing Namer 3000, 100XP
+Console.WriteLine("What kind of thing are we talking about?"); //ask user the question 
+string a = Console.ReadLine();  //store the answer in string variable
+Console.WriteLine("How would you describe it? Big? Azure? Tattered? "); //ask another question
+string b = Console.ReadLine(); //store the answer in the string variable
+string c = " of Doom";
+string d = " 3000! "; //assigning value to variable
+Console.WriteLine("The " + b + " " + a + c + d); //print out all the string variable 
+//I would rather give a clear name to string variable for ex string a to string type, string describtion 
+
+//challenge 
