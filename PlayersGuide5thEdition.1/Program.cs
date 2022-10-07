07862ca1083b6f5ba17a059f50bef9e5d6e75235
KrittapatBerg <99590760+KrittapatBerg@users.noreply.github.com>
2022-10-07 11:38:00 +0200

    up to chapter7

--- PlayersGuide5thEdition.1/Program.cs
+++ PlayersGuide5thEdition.1/Program.cs
@@ -2,32 +2,61 @@
 
 using System.Data;
 
-Console.WriteLine(" Hello, World! ");
+//Console.WriteLine(" Hello, World! ");
 
-//challenge What Comes Next 50XP
-Console.WriteLine("My name is Quan.");
+////challenge What Comes Next 50XP
+//Console.WriteLine("My name is Quan.");
 
-//challenge The Makings of a Programmer 50XP
-Console.WriteLine("I have come from the far away land... ");
-Console.WriteLine("This place is such a beautiful place. ");
-Console.WriteLine("Do you mind answering my question? ");
-Console.WriteLine("What is your name? ");
-string name = Console.ReadLine();
-Console.WriteLine(" Hello " + name + "! Nice to meet you! ");
+////challenge The Makings of a Programmer 50XP
+//Console.WriteLine("I have come from the far away land... ");
+//Console.WriteLine("This place is such a beautiful place. ");
+//Console.WriteLine("Do you mind answering my question? ");
+//Console.WriteLine("What is your name? ");
+//string name = Console.ReadLine();
+//Console.WriteLine(" Hello " + name + "! Nice to meet you! ");
 
-//challenge Consolas and Telim 50XP 
-Console.WriteLine("\nBread is ready.\n Who is the bread for? "); 
-string breadTaker = Console.ReadLine();
-Console.WriteLine("Noted: " + breadTaker + " got bread.");
+////challenge Consolas and Telim 50XP 
+//Console.WriteLine("\nBread is ready.\n Who is the bread for? "); 
+//string breadTaker = Console.ReadLine();
+//Console.WriteLine("Noted: " + breadTaker + " got bread.");
 
-//challenge The Thing Namer 3000, 100XP
-Console.WriteLine("What kind of thing are we talking about?"); //ask user the question 
-string a = Console.ReadLine();  //store the answer in string variable
-Console.WriteLine("How would you describe it? Big? Azure? Tattered? "); //ask another question
-string b = Console.ReadLine(); //store the answer in the string variable
-string c = " of Doom";
-string d = " 3000! "; //assigning value to variable
-Console.WriteLine("The " + b + " " + a + c + d); //print out all the string variable 
-//I would rather give a clear name to string variable for ex string a to string type, string describtion 
+////challenge The Thing Namer 3000, 100XP
+//Console.WriteLine("What kind of thing are we talking about?"); //ask user the question 
+//string a = Console.ReadLine();  //store the answer in string variable
+//Console.WriteLine("How would you describe it? Big? Azure? Tattered? "); //ask another question
+//string b = Console.ReadLine(); //store the answer in the string variable
+//string c = " of Doom";
+//string d = " 3000! "; //assigning value to variable
+//Console.WriteLine("The " + b + " " + a + c + d); //print out all the string variable 
+////I would rather give a clear name to string variable for ex string a to string type, string describtion 
 
-//challenge 
+//challenge The Variable Shop 100XP
+string firstName = "Gorge";
+Console.WriteLine(firstName);
+
+int age = 50;
+Console.WriteLine(firstName + " is " + age + " years old");
+
+char initial = 'G';
+Console.WriteLine("His initial is " + initial);
+
+float favNum = 99.998f;
+Console.WriteLine("His favorite number is " + favNum);
+
+double salary = 45000.75;
+Console.WriteLine("He earns " + salary + " per month");
+
+decimal bodyTemp = 37.6m;
+Console.WriteLine("His body temperature is at " + bodyTemp + " degree C");
+
+bool gorge = true;
+
+//challeng the Variable Shop Returns 50XP 
+firstName = "Jeremiah";
+age = 29;
+initial = 'J';
+favNum = 300.456f;
+salary = 34589.45;
+bodyTemp = 39.1m;
+gorge = false;
+Console.WriteLine(firstName, age, initial, favNum, salary, bodyTemp); 
